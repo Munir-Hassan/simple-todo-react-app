e015920ca769c3118588a54c4954f4663ae158e9
Munir-Hassan <xvidplasma@gmail.com>
2020-08-01 01:55:09 +0600

    Updates

---
+++ precache-manifest.0686bc211215e712004daa4fb3dc6f53.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6b0a202211f5e022d65632f0f5c3eb7",
+    "url": "/Munir-Hassan/simple-todo-react-app/index.html"
+  },
+  {
+    "revision": "f2f6fa4bdbdc85992bc1",
+    "url": "/Munir-Hassan/simple-todo-react-app/static/css/main.af2d40e4.chunk.css"
+  },
+  {
+    "revision": "0e9275c83d3f56bfcf30",
+    "url": "/Munir-Hassan/simple-todo-react-app/static/js/2.22965e46.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Munir-Hassan/simple-todo-react-app/static/js/2.22965e46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f2f6fa4bdbdc85992bc1",
+    "url": "/Munir-Hassan/simple-todo-react-app/static/js/main.97afd8b7.chunk.js"
+  },
+  {
+    "revision": "ca674f3a57b3a73c71dd",
+    "url": "/Munir-Hassan/simple-todo-react-app/static/js/runtime-main.7151886a.js"
+  }
+]);
