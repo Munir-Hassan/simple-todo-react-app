2309c9bcb583e1f2841d46f0066ffd2aeaea6218
Munir-Hassan <xvidplasma@gmail.com>
2020-07-31 21:00:45 +0600

    first commit

--- src/components/InputTodo.js
+++ src/components/InputTodo.js
@@ -20,15 +20,16 @@ class InputTodo extends React.Component {
 	};
 	render() {
 		return (
-			<form onSubmit={this.handleSubmit}>
+			<form onSubmit={this.handleSubmit} className="form-container">
 				<input
 					type="text"
+					className="input-text"
 					placeholder="Add Todo..."
 					value={this.state.title}
 					name="title"
 					onChange={this.addInput}
 				/>
-				<input type="submit" value="Submit" />
+				<input type="submit" className="input-submit" value="Submit" />
 			</form>
 		);
 	}
--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -2,16 +2,20 @@ import React from 'react';
 
 class TodoItem extends React.Component {
 	render() {
+		const completedStyle = {
+			fontStyle: 'italic',
+			color: '#d35e0f',
+			opacity: 0.4,
+			textDecoration: 'line-through'
+		};
+
+		const { completed, id, title } = this.props.todo;
 		return (
 			<div>
-				<li>
-					<input
-						type="checkbox"
-						checked={this.props.todo.completed}
-						onChange={() => this.props.handleChangeProps(this.props.todo.id)}
-					/>
-					<button onClick={() => this.props.deleteTodoProps(this.props.todo.id)}>Delete</button>
-					{this.props.todo.title}
+				<li className="todo-item">
+					<input type="checkbox" checked={completed} onChange={() => this.props.handleChangeProps(id)} />
+					<button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
+					<span style={completed ? completedStyle : null}>{title}</span>
 				</li>
 			</div>
 		);
